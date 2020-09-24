99a465382389396defec0a9bf52f127c292972ce
Timothy Belmore <timothybelmore@trinitybiblecollege.edu>
2020-09-23 23:52:24 -0600

    Basic API responses logged in each field for each one possible in the standard 5-day/3hr api.

--- script.js
+++ script.js
@@ -16,7 +16,76 @@ $(document).ready(function () {
 
 
             console.log(response)
+
+
+            function dayOne() {
+
+                // $("#day-1-name").text(response.)
+                $("#day-1-date").text("Date:" + response.list[2].dt_txt)
+                $("#day-1-temp").text("Temp:" + response.list[2].main.temp)
+                $("#day-1-humidity").text("Humidity:" + response.list[2].main.humidity)
+                $("#day-1-wind").text("Windspeed:" + response.list[2].wind.speed + "km/hr")
+                // $("#")
+            }
+
+            dayOne()
+
+            function dayTwo() {
+
+                // $("#day-2-name").text(response.)
+                $("#day-2-date").text("Date:" + response.list[10].dt_txt)
+                $("#day-2-temp").text("Temp:" + response.list[10].main.temp)
+                $("#day-2-humidity").text("Humidity:" + response.list[10].main.humidity)
+                $("#day-2-wind").text("Windspeed:" + response.list[10].wind.speed + "km/hr")
+                // $("#")
+            }
+
+            dayTwo()
+
+            function dayThree() {
+
+                // $("#day-1-name").text(response.)
+                $("#day-3-date").text("Date:" + response.list[18].dt_txt)
+                $("#day-3-temp").text("Temp:" + response.list[18].main.temp)
+                $("#day-3-humidity").text("Humidity:" + response.list[18].main.humidity)
+                $("#day-3-wind").text("Windspeed:" + response.list[18].wind.speed + "km/hr")
+                // $("#")
+            }
+
+            dayThree()
+
+            function dayFour() {
+
+                // $("#day-1-name").text(response.)
+                $("#day-4-date").text("Date:" + response.list[26].dt_txt)
+                $("#day-4-temp").text("Temp:" + response.list[26].main.temp)
+                $("#day-4-humidity").text("Humidity:" + response.list[26].main.humidity)
+                $("#day-4-wind").text("Windspeed:" + response.list[26].wind.speed + "km/hr")
+                // $("#")
+            }
+
+            dayFour()
+
+            function dayFive() {
+
+                // $("#day-1-name").text(response.)
+                $("#day-5-date").text("Date:" + response.list[34].dt_txt)
+                $("#day-5-temp").text("Temp:" + response.list[34].main.temp)
+                $("#day-5-humidity").text("Humidity:" + response.list[34].main.humidity)
+                $("#day-5-wind").text("Windspeed:" + response.list[34].wind.speed + "km/hr")
+                // $("#")
+            }
+
+            dayFive()
+
         });
+
+
+
+
+
+
+
 
 
 
