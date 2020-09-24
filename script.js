26f7a1913d1a2d686a79d1cf89c0547043a2ab3c
Timothy Belmore <timothybelmore@trinitybiblecollege.edu>
2020-09-23 20:27:56 -0600

    Added an input/button where the city entered is placed into the API call and becomes the chosen City. Also fixed some scoping issues.

--- script.js
+++ script.js
@@ -1,46 +1,26 @@
 let apiKey = "d3cbd8bd94e3ee7832b4bc0d9b352bf0"
-let citySearch = "Helena"
-let queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + citySearch + "&appid=" + apiKey
 
 $(document).ready(function () {
 
+    $("#city-input-btn").on("click", function (event) {
+        event.preventDefault()
+        let citySearch = $("#city-input").val()
+        let queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + citySearch + "&appid=" + apiKey
 
 
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function (response) {
-
-
-
-        console.log(response)
-    });
-
-
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
 
 
 
+            console.log(response)
+        });
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    })
 
 
 })
